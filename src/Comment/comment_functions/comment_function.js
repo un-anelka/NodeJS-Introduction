1123569dbfceb34008c64571232c315080023827
andela <andela@andelas-MacBook-Pro.local>
2022-03-30 10:48:10 +0200

    Modify Procfile

--- src/Comment/comment_functions/comment_function.js
+++ src/Comment/comment_functions/comment_function.js
@@ -1,50 +1,50 @@
 import usersModel from "../../Usersfolder/users_Schema.js";
-import blogsModel from "../../Blogs/blogs_Schema.js";
+import blogsModel from "../../src/Blogs/blogs_Schema.js";
 
 
 
 
-const commentPost= async(req, res) => {
-    try {
-      if (!req.body) return res.json({message: "empty"})
-      console.log(req.body)
-        const id = req.params.id
-        const blogs = await blogsModel.findOneAndUpdate({_id:id},{
-        // const blogs = await blogsModel.findById(id)
-        // blogs({
-          $addToSet:{
-            comments: {
-                username:"UN RUDASINGWA",
-                data: req.body,
-                date: Date.now()
-            }
-          }
-        })
-       
-        // res.send(blogs)
-        res.status(201).json({
-            message: "comment has been created successfully",
-        })
-    } catch (error) {
-        console.log(error)
-    }; 
+const commentPost = async (req, res) => {
+  try {
+    if (!req.body) return res.json({ message: "empty" })
+    console.log(req.body)
+    const id = req.params.id
+    const blogs = await blogsModel.findOneAndUpdate({ _id: id }, {
+      // const blogs = await blogsModel.findById(id)
+      // blogs({
+      $addToSet: {
+        comments: {
+          username: "UN RUDASINGWA",
+          data: req.body,
+          date: Date.now()
+        }
+      }
+    })
+
+    // res.send(blogs)
+    res.status(201).json({
+      message: "comment has been created successfully",
+    })
+  } catch (error) {
+    console.log(error)
+  };
 }
 
-const commentDelete= async(req, res) => {
+const commentDelete = async (req, res) => {
   try {
     const id = req.params.id;
     const blogId = await blogsModel.findByIdAndDelete(id);
     if (!blogId) {
-        return res.json({ message: "The blog you are trying to delete does not exist." })
+      return res.json({ message: "The blog you are trying to delete does not exist." })
     }
 
     res.status(200).json({
-        message: `blog with the ID:${id} was deleted successfully`,
-        // data: blogId
+      message: `blog with the ID:${id} was deleted successfully`,
+      // data: blogId
     })
-} catch (error) {
+  } catch (error) {
     console.log(error)
-};
+  };
 
 }
-export {commentPost as default}
+export { commentPost as default }
